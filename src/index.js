0ee9a074ddc41ddffd641beed0d7bc5bd7268be5
Wesley Van Komen <WesleyVanKomen@gmail.com>
2019-05-31 22:54:50 -0700

    fill in component outline and add onClick event to lines.

--- src/index.js
+++ src/index.js
@@ -2,29 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const cellSize = 100;
+const lineWidth = 25;
+
 class Grid extends React.Component { 
     render(props) {
 
+        console.log(this.props.gridState);
+
+        var columns = this.props.settings.columns;
+
+        var width = parseInt(columns / 2) * cellSize + parseInt(columns / 2 + 1) * lineWidth + columns
+
         var rowsArr = [];
+        var cellWidth, cellHeight, cellSelectMethod
         
 		for (var i = 0; i < this.props.settings.rows; i++) {
 			for (var j = 0; j < this.props.settings.columns; j++) {
 
-				rowsArr.push(
-					<Cell
-                        className="cell"
-						row={i}
-						column={j}
-                        width="16"
-                        color="blue"
-						selectBox={this.props.selectBox}
-					/>
-				);
+                cellSelectMethod = this.props.selectBox;
+
+                // corner box (i % 2 == 0) && (j % 2 == 0)
+                if ((i % 2 === 0) && (j % 2 === 0)){
+                    cellWidth = lineWidth;
+                    cellHeight = lineWidth;
+                    cellSelectMethod = () => {};
+                }
+                // line (i % 2 == 1) || (J % 2 == 1)
+                else if (i % 2 === 1 && j % 2 === 0){
+                    cellWidth = lineWidth;
+                    cellHeight = cellSize;
+                }
+                else if (i % 2 === 0 && j % 2 === 1){
+                    cellWidth = cellSize;
+                    cellHeight = lineWidth;
+                }
+                // point box (i % 2 == 1) && ( j % 2 == 1)
+                else if ((i % 2 === 1) && ( j % 2 === 1)){
+                    cellWidth = cellSize;
+                    cellHeight = cellSize;
+                    cellSelectMethod = () => {};
+                }
+
+                rowsArr.push(
+                    <Cell
+                        key={i + "_" + j}
+                        row={i}
+                        column={j}
+                        width={cellWidth}
+                        height={cellHeight}
+                        color={this.props.gridState[i][j]}
+                        selectBox={cellSelectMethod}
+                    />
+                );
 			}
 		}
 
 		return (
-			<div className="grid">
+			<div className="grid" style={{width: width}}>
 				{rowsArr}
 			</div>
 		);
@@ -32,9 +67,16 @@ class Grid extends React.Component {
 }
 
 class Cell extends React.Component {
+    selectBox = () => {
+        this.props.selectBox(this.props.row, this.props.column, "green");
+	}
+    
     render(props) {
         return (
-            <div className="cell" style={{backgroundColor: this.props.color}} />
+            <div 
+                className="cell" 
+                style={{backgroundColor: this.props.color, width: this.props.width, height: this.props.height}}
+                onClick={this.selectBox} />
         )
     }
 }
@@ -44,26 +86,36 @@ class Main extends React.Component {
         super(props);
         this.playing = true;
         this.victory = false;
-        this.settings = this.props.settings;
-        this.playerOne = this.props.playerOne;
-        this.playerTwo = this.props.playerTwo;
-
         this.state = {
             round: 1,
-            gameGrid: Array(this.rows).fill().map(() => Array(this.columns).fill(false))
+            gridState: Array(this.props.settings.rows).fill().map(() => Array(this.props.settings.columns).fill("white"))
         }
+        console.log(this.state.gridState);
     }
 
     render() {
         return(
-            <div>
+            <div style={{width: "100%"}}>
                 <h1>Lines and Boxes</h1>
                 <h2>Round {this.state.round}</h2>
-                <Grid settings={this.props.settings}/>
+                <Grid settings={this.props.settings} selectBox={this.selectBox} gridState = {this.state.gridState}/>
                 
             </div>
         )
     }
+
+    selectBox = (row, col, color) => {
+        console.log(row + "," + col, " selected " + color)
+		let gridCopy = arrayClone(this.state.gridState);
+		gridCopy[row][col] = color;
+		this.setState({
+			gridState: gridCopy
+		});
+    }
 }
 
-ReactDOM.render(<Main settings={{ rows : 12, columns : 12 }} playerOne={{ score : 0, color: "red" }} playerTwo={{ score : 0, color: "blue" }} />, document.getElementById('root'))
+function arrayClone(arr) {
+	return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main settings={{ rows : 7, columns : 7 }} playerOne={{ score : 0, color: "red" }} playerTwo={{ score : 0, color: "blue" }} />, document.getElementById('root'))
